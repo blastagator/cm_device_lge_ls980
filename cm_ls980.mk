0dd142d3bc1735a9740ba916013cb2f8c8f97f06
Greg Wallace <greg@gregtwallace.com>
2014-12-06 16:09:48 -0500

    Add AndroidProducts. Slight renaming.

--- cm_ls980.mk
+++ cm_ls980.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/ls980/ls980.mk)
+$(call inherit-product, device/lge/ls980/full_ls980.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ls980
